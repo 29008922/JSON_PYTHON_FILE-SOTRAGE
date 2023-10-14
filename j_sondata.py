e04083112d7adb908a43dd114a72aec3631198cb
29008922 <haron.murumba22@gmail.com>
2023-10-14 07:00:41 -0700

    json file

---
+++ j_sondata.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+# Import the module
+import json
+
+# String with JSON format
+data_JSON =  """
+{
+	"size": "Medium",
+	"price": 15.67,
+	"toppings": ["Mushrooms", "Extra Cheese", "Pepperoni", "Basil"],
+	"client": {
+		"name": "Jane Doe",
+		"phone": "455-344-234",
+		"email": "janedoe@email.com"
+	}
+}
+"""
+
+# Convert JSON string to dictionary
+data_dict = json.loads(data_JSON)
+
+print(data_dict["size"])
+print(data_dict["price"])
+print(data_dict["toppings"])
+print(data_dict["client"])
